a1811eff65effb75e1bd15c8985f08a5d067bd70
dlavareda <diogolavareda9@hotmail.com>
2019-09-18 19:54:24 +0100

    Update

--- Hello World/src/hello/world/HelloWorld.java
+++ Hello World/src/hello/world/HelloWorld.java
@@ -15,8 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-    }
-    
+        for (int i = 0; i < 10; i++) {
+          System.out.println("Hello World " + i);
+        }
+    }    
 }
